48b778588e43d4c5c69febe8be1ac42e337a217a
Mohammad Sheikh <msheik24@uwo.ca>
2021-06-26 00:59:42 -0400

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  extends: [
+    "airbnb-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "prettier",
+  ],
+  rules: {
+    "no-prototype-builtins": "off",
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    "no-use-before-define": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+      },
+    },
+  ],
+};
--- src/index.test.ts
+++ src/index.test.ts
@@ -3,3 +3,4 @@ import { sum } from './index'
 test('sum', () => {
   expect(sum(1, 2)).toBe(3)
 })
+
