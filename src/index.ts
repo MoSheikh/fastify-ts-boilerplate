4ae9ac2ff70c755575384bfc51c7c7a59bf28d75
Mohammad Sheikh <msheik24@uwo.ca>
2021-06-27 19:34:01 -0400

    Add ts-node as hot-reload server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { server } from "./server.js";
 import { env } from "./environment.js";
 
-server.listen(env.PORT, (err, address) => {
+server.listen(env.PORT, (err) => {
   if (err) {
     server.log.error(err);
     process.exit(1);
   }
-  server.log.info(`Server listening on ${address}`);
 });
